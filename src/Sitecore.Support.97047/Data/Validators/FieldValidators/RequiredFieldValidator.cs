c8dfc5f72bb628dd88dd0dd7c7ed556e8b6e5aae
DK\DTAT <dtat@sitecore.net>
2017-07-18 13:04:48 +0300

    Make the 'RequiredFieldValidator' class serializable.

--- src/Sitecore.Support.97047/Data/Validators/FieldValidators/RequiredFieldValidator.cs
+++ src/Sitecore.Support.97047/Data/Validators/FieldValidators/RequiredFieldValidator.cs
@@ -1,45 +1,60 @@
 ﻿namespace Sitecore.Support.Data.Validators.FieldValidators
 {
-    using Sitecore.Data.Validators;
-    using System;
-    using System.Runtime.Serialization;
+  using Sitecore.Data.Validators;
+  using System;
+  using System.Runtime.Serialization;
 
-    public class RequiredFieldValidator : StandardValidator
+  [Serializable]
+  public class RequiredFieldValidator : StandardValidator
+  {
+    #region Original code
+
+    public RequiredFieldValidator()
     {
-        public RequiredFieldValidator()
-        {
-        }
-
-        protected RequiredFieldValidator(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
-
-        protected override ValidatorResult Evaluate()
-        {
-            if (!string.IsNullOrEmpty(base.ControlValidationValue))
-            {
-                return ValidatorResult.Valid;
-            }
-            if (base.GetItem() == null)
-            {
-                return ValidatorResult.Unknown;
-            }
-            string[] arguments = new string[] { base.GetFieldDisplayName() };
-            base.Text = base.GetText("Field \"{0}\" must contain a value.", arguments);
-            return base.GetFailedResult(ValidatorResult.CriticalError);
-        }
-
-        protected override ValidatorResult GetMaxValidatorResult()
-        {
-           return base.GetFailedResult(ValidatorResult.CriticalError);
-        }
-
-        public override string Name 
-        {
-            get
-            {
-               return "Required";
-            }
-        }
     }
+
+    protected RequiredFieldValidator(SerializationInfo info, StreamingContext context)
+      : base(info, context)
+    {
+    }
+
+    #endregion
+
+    #region Modified code
+
+    protected override ValidatorResult Evaluate()
+    {
+      if (!string.IsNullOrEmpty(base.ControlValidationValue))
+      {
+        return ValidatorResult.Valid;
+      }
+
+      if (base.GetItem() == null)
+      {
+        return ValidatorResult.Unknown;
+      }
+
+      string[] arguments = new string[] { base.GetFieldDisplayName() };
+      base.Text = base.GetText("Field \"{0}\" must contain a value.", arguments);
+      return base.GetFailedResult(ValidatorResult.CriticalError);
+    }
+    #endregion
+
+    #region Original code
+
+    protected override ValidatorResult GetMaxValidatorResult()
+    {
+      return base.GetFailedResult(ValidatorResult.CriticalError);
+    }
+
+    public override string Name
+    {
+      get
+      {
+        return "Required";
+      }
+    }
+
+    #endregion
+  }
 }
--- src/Sitecore.Support.97047/Hooks/UpdateNumberFieldDefinitionItem.cs
+++ src/Sitecore.Support.97047/Hooks/UpdateNumberFieldDefinitionItem.cs
@@ -1,41 +1,41 @@
 ﻿namespace Sitecore.Support.Hooks
 {
-    using Sitecore.Configuration;
-    using Sitecore.Diagnostics;
-    using Sitecore.Events.Hooks;
-    using Sitecore.SecurityModel;
-    using System;
+  using Sitecore.Configuration;
+  using Sitecore.Diagnostics;
+  using Sitecore.Events.Hooks;
+  using Sitecore.SecurityModel;
+  using System;
 
-    public class UpdateNumberFieldDefinitionItem : IHook
+  public class UpdateNumberFieldDefinitionItem : IHook
+  {
+    public void Initialize()
     {
-        public void Initialize()
+      using (new SecurityDisabler())
+      {
+        var databaseName = "master";
+        var itemPath = "/sitecore/system/Settings/Validation Rules/Field Rules/Required";
+        var fieldName = "Type";
+
+        var type = typeof(Sitecore.Support.Data.Validators.FieldValidators.RequiredFieldValidator);
+
+        var typeName = type.FullName;
+        var assemblyName = type.Assembly.GetName().Name;
+        var fieldValue = string.Format("{0}, {1}", typeName, assemblyName);
+
+        var database = Factory.GetDatabase(databaseName);
+        var item = database.GetItem(itemPath);
+
+        if (string.Equals(item[fieldName], fieldValue, StringComparison.Ordinal))
         {
-            using (new SecurityDisabler())
-            {
-                var databaseName = "master";
-                var itemPath = "/sitecore/system/Settings/Validation Rules/Field Rules/Required";
-                var fieldName = "Type";
-
-                var type = typeof(Sitecore.Support.Data.Validators.FieldValidators.RequiredFieldValidator);
-
-                var typeName = type.FullName;
-                var assemblyName = type.Assembly.GetName().Name;
-                var fieldValue = string.Format("{0}, {1}", typeName, assemblyName);
-
-                var database = Factory.GetDatabase(databaseName);
-                var item = database.GetItem(itemPath);
-
-                if (string.Equals(item[fieldName], fieldValue, StringComparison.Ordinal))
-                {
-                    return;
-                }
-
-                Log.Info(string.Format("Installing {0}", assemblyName), this);
-
-                item.Editing.BeginEdit();
-                item[fieldName] = fieldValue;
-                item.Editing.EndEdit();
-            }
+          return;
         }
+
+        Log.Info(string.Format("Installing {0}", assemblyName), this);
+
+        item.Editing.BeginEdit();
+        item[fieldName] = fieldValue;
+        item.Editing.EndEdit();
+      }
     }
+  }
 }
